f648f05d7fffb85c8ff82513415d5b5195863cee
kaosbeat <kasper.jordaens@gmail.com>
2016-05-06 08:06:15 +0200

    ignoring target

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to bossig
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ src/bossig/core.clj
@@ -0,0 +1,12 @@
+(ns bossig.core
+  (:use [overtone.live]
+        [overtone.inst.piano])
+
+  )
+
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (println x "Hello, World!"))
+
+(piano)
